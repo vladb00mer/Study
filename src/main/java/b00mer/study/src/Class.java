8952a066301e0019230d309ad284357fed23f812
b00mer <b00mer@vladb00mer>
2016-04-17 09:13:52 +0300

    class Class

---
+++ src/main/java/b00mer/study/src/Class.java
@@ -0,0 +1,43 @@
+package b00mer.study.src;
+
+public class Class {
+
+    private int classID;
+    private String className;
+    private int classChiefID;
+    
+    public Class(int id, String name, int chiefID) {
+    
+        setClassID(id);           
+        setClassName(name);        
+        setClassChiefID(chiefID);        
+                
+    }
+
+    public int getClassID() {
+        return classID;
+    }
+
+    public final void setClassID(int classID) {
+        this.classID = classID;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public final void setClassName(String className) {
+        this.className = className;
+    }
+
+    public int getClassChiefID() {
+        return classChiefID;
+    }
+
+    public final void setClassChiefID(int classChiefID) {
+        this.classChiefID = classChiefID;
+    }
+    
+    
+    
+}
