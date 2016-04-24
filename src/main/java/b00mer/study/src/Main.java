65d5e1a4ae576b2216bc3e80fc159cc4f69744ad
b00mer <b00mer@vladb00mer>
2016-04-24 10:45:14 +0300

    equaled to DB

--- src/main/java/b00mer/study/src/Main.java
+++ src/main/java/b00mer/study/src/Main.java
@@ -1,10 +1,13 @@
 package b00mer.study.src;
 
+import b00mer.study.gui.MainWindow;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        MainWindow mainWindow = new MainWindow();
 
+        mainWindow.setVisible(true);
     }
-    
 }
