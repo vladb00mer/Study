a941a9bf31502ab2c3e16ca089edd730ac617377
b00mer <b00mer@vladb00mer>
2016-05-06 09:33:16 +0300

    change connector

--- src/main/java/b00mer/study/connector/DatabaseConnector.java
+++ src/main/java/b00mer/study/connector/DatabaseConnector.java
@@ -1,6 +1,7 @@
 package b00mer.study.connector;
 
 import javax.ejb.Stateful;
+import javax.jms.Connection;
 
 @Stateful
 public class DatabaseConnector {
@@ -8,13 +9,14 @@ public class DatabaseConnector {
     private String dbName;
     private String dbLogin;
     private String dbPassword;
+    
+    private Connection dbConnection;
        
     public DatabaseConnector(String name, String login, String password) {
     
         setDbName(name);
         setDbLogin(login);   
-        setDbPassword(password);
-                
+        setDbPassword(password);                
     }
 
     public String getDbName() {
@@ -39,5 +41,19 @@ public class DatabaseConnector {
 
     public final void setDbPassword(String dbPassword) {
         this.dbPassword = dbPassword;
+    }
+
+    /**
+     * @return the dbConnection
+     */
+    public Connection getDbConnection() {
+        return dbConnection;
+    }
+
+    /**
+     * @param dbConnection the dbConnection to set
+     */
+    public void setDbConnection(Connection dbConnection) {
+        this.dbConnection = dbConnection;
     }
 }
