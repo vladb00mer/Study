774427bffe1c8be0fb5ca98232a81d7470b20f90
b00mer <b00mer@vladb00mer>
2016-08-05 09:59:04 +0300

    chief test ready

--- src/main/java/b00mer/study/model/Main.java
+++ src/main/java/b00mer/study/model/Main.java
@@ -9,12 +9,7 @@ public class Main {
 
     DatabaseConnector databaseConnector = new DatabaseConnector();
     
-    /*databaseConnector.putQuery("insert into chief values ('ch000001', 'Ivanov', 'Ivan', 'Petrovich', 120000);");
-    Chief chief = new Chief("select * from chief where chief_salary = 120000");
-    System.out.println(chief.toString());    
-    databaseConnector.putQuery("delete from chief where chief_id = 'ch000001';");
-    
-    databaseConnector.putQuery("insert into student values ('st0001', 'Petr', 'Petrov', 'Ivanovich', 'cl0001');");    
+    /*databaseConnector.putQuery("insert into student values ('st0001', 'Petr', 'Petrov', 'Ivanovich', 'cl0001');");    
     Student student = new Student("select * from student where student_id = 'st0001';");    
     System.out.println(student.toString());    
     databaseConnector.putQuery("delete from student where student_id = 'st0001';");
@@ -22,12 +17,12 @@ public class Main {
     databaseConnector.putQuery("insert into course values ('co0001', 'Math', 120, 'ch0001');");    
     Course course = new Course("select * from course where course_id = 'co0001';");    
     System.out.println(course.toString());    
-    databaseConnector.putQuery("delete from course where course_id = 'co0001';");*/
+    databaseConnector.putQuery("delete from course where course_id = 'co0001';");
     
     databaseConnector.putQuery("insert into class values ('cl0001', 'co0001', 'ch0001');");    
     Group group = new Group("select * from class where class_id = 'cl0001';");    
     System.out.println(group.toString());    
-    databaseConnector.putQuery("delete from class where class_id = 'cl0001';");
+    databaseConnector.putQuery("delete from class where class_id = 'cl0001';");*/
 
     databaseConnector.closeMySQLConnection();
     
---
+++ src/test/java/b00mer/study/unitTests/ChiefTest.java
@@ -0,0 +1,42 @@
+package b00mer.study.unitTests;
+
+import b00mer.study.connector.DatabaseConnector;
+import b00mer.study.model.Chief;
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ChiefTest {
+    
+    DatabaseConnector databaseConnector;
+        
+    @Before
+    public void setUp() throws SQLException {
+    
+        databaseConnector = new DatabaseConnector();
+        
+        databaseConnector.putQuery("delete from chief where chief_id = 'ch000001';");
+        databaseConnector.putQuery("insert into chief values ('ch000001', 'Ivanov', 'Ivan', 'Petrovich', 120000);");
+    }
+    
+    @After
+    public void tearDown() throws SQLException {
+        
+        databaseConnector.putQuery("delete from chief where chief_id = 'ch000001';");
+        databaseConnector.closeMySQLConnection();
+    }
+
+    @Test
+    public void test() throws SQLException {
+    
+        Chief chief = new Chief("select * from chief where chief_id = 'ch000001';");
+
+        Assert.assertTrue(chief.getChiefID().equals("ch000001"));
+        Assert.assertTrue(chief.getChiefLastName().equals("Ivanov"));
+        Assert.assertTrue(chief.getChiefName().equals("Ivan"));
+        Assert.assertTrue(chief.getChiefMidName().equals("Petrovich"));    
+        Assert.assertTrue(chief.getChiefSalary() == 120000);
+    }
+}
