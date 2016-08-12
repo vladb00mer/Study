9552edc387e644d2a86d6884ee4333eb3b19c801
b00mer <b00mer@vladb00mer>
2016-08-12 09:21:04 +0300

    seems to be ready

--- src/main/java/b00mer/study/model/Main.java
+++ src/main/java/b00mer/study/model/Main.java
@@ -1,15 +1,5 @@
 package b00mer.study.model;
 
-import b00mer.study.connector.DatabaseConnector;
-import java.sql.SQLException;
-
-public class Main {    
+public class Main {  
     
-    public static void main(String[] args) throws SQLException {
-
-    DatabaseConnector databaseConnector = new DatabaseConnector();
-    
-    databaseConnector.closeMySQLConnection();
-    
-    }
 }
